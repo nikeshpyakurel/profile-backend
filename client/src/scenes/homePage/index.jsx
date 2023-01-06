57664c7514513e75b07df0c68409c235ae4c96c3
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-01-06 17:10:04 +0545

    redux setup and nav component done

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import NavBar from "../navBar";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  );
+};
+
+export default HomePage;
